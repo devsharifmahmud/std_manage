57f006281171f9dd74fdcb8b3d56371823bf4560
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-03-24 15:26:38 +0600

    Update code

--- std/views.py
+++ std/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def home(request):
     return render(request, 'std/home.html', {})
 
 def std_add(request):
-    if models.method == 'POST':
-
+    if request.method == 'POST':
+        print('Added')
     return render(request, 'std/std_add.html', {})
