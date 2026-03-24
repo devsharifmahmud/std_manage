ba80bd49d2b89c1db2fa4807b6c76c9183f432ff
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-03-24 15:34:46 +0600

    Update code

--- std/views.py
+++ std/views.py
@@ -8,4 +8,8 @@ def std_add(request):
     if request.method == 'POST':
         print('Added')
         std_roll = request.POST.get('roll')
+        std_name = request.POST.get('name')
+        std_email = request.POST.get('email')
+        std_phone = request.POST.get('phone')
+        std_address = request.POST.get('address')
     return render(request, 'std/std_add.html', {})
